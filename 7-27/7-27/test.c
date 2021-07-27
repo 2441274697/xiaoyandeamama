b4e08ddfdff0930d5734a6e0a8642895fb0708d6
2441274697 <2441274697@qq.com>
2021-07-27 20:35:09 +0800

    7-27

---
+++ 7-27/7-27/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+
+//int main()
+//{
+//	//字符指针：
+//	char ch = 'w';
+//	char* pc = &ch;
+//	const char* p = "abcdef";
+//	int* p3;//整型指针-指向整型的指针
+//	char* p4;//字符指针-指向字符的指针
+//	//指针数组-数组-存放指针的数组
+//	int* arr[10];
+//	char* p2[5];
+//	//指针数组指向数组的指针
+//	int arr2[5];
+//	int(*parr)[5]=&arr;
+//	return 0;
+//}
