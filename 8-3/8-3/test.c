1d39a490d212ffa2ac905bd250b75286a3381664
2441274697 <2441274697@qq.com>
2021-08-05 11:12:03 +0800

    8-3

---
+++ 8-3/8-3/test.c
@@ -0,0 +1,204 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+#include<math.h>
+
+
+//求0-100000的自幂数
+//自幂数：一个n位数的i，i的每一位的n次方等于i。
+//这样的i是自幂数
+//int main()
+//{
+//	int i = 0;
+//	//1-100000之间的数
+//	for (i = 0; i < 100000; i++)
+//	{
+//		//1.求i是n位数
+//		int n = 1;
+//		int tmp = i;
+//		int sum = 0;
+//		while (tmp /= 10)
+//		{
+//			n++;
+//		}
+//		//2.计算每一位的次方和
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += (int)pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//		//3.比较
+//		if (sum == i)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+////求1-100000之间所有的自幂数
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 100000; i++)
+//	{
+//		int n = 1;
+//		int tmp = i;
+//		int sum = 0;
+//		while (tmp /= 10)
+//		{
+//			n++;
+//		}
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d%d", &a, &n);
+//	int sum = 0;
+//	int i = 0;
+//	int ret = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+////写一个函数能够逆序字符串的内容
+//
+//
+//void reverse(char* str)
+//{
+//	assert(str);
+//	int len = strlen(str);
+//	char* left = str;
+//	char* right = str + len - 1;
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[256] = { 0 };
+//	/*scanf("%s", arr);*/
+//	gets(arr);
+//	reverse(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//void reverse(char* str)
+//{
+//	assert(str);
+//	int len = strlen(str);
+//	char* left = str;
+//	char* right = str + len - 1;
+//	while (left < right)
+//	{
+//		char tmp = *right;
+//		*right = *left;
+//		*left = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+
+//int main()
+//{
+//	char arr[256] = { 0 };
+//	scanf("%s", arr);
+//	//逆序函数
+//	reverse(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//int main()
+//{
+//	unsigned long pulArray[] = { 6, 7, 8, 9, 10 };
+//	unsigned long *pulPtr;
+//	pulPtr = pulArray;
+//	*(pulPtr + 3) += 3;
+//	printf("%d,%d\n", *pulPtr, *(pulPtr + 3));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char *c[] = { "ENTER", "NEW", "POINT", "FIRST" };
+//	char* *cp[] = { c + 3, c + 2, c + 1, c };
+//	char** *cpp = cp;
+//	printf("%s\n", **++cpp);
+//	printf("%s\n", *--*++cpp + 3);
+//	printf("%s\n", *cpp[-2] + 3);
+//	printf("%s\n", cpp[-1][-1] + 1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//指针数组，数组中的每个元素为字符指针
+//	char *a[] = { "work", "at", "alibaba" };
+//	char* *pa = a;
+//	pa++;
+//	printf("%s\n", *pa);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[3][2] = { (0, 2), (2, 3), (4, 5) };
+//	int *p;
+//	p = a[0];
+//	printf("%d", p[0]);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[4] = { 1, 2, 3, 4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//	printf("%x,%x", ptr1[-1], *ptr2);
+//	//       
+//	return 0;
+//}
